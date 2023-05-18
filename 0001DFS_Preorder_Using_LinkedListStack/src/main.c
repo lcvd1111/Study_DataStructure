9f2422a491f79aceb11d72a40366cdb3f357c6af
lcvd1111 <lcvd1111@gmail.com>
2023-05-18 10:06:53 +0900

    .

--- 0001DFS_Preorder_Using_LinkedListStack/src/main.c
+++ 0001DFS_Preorder_Using_LinkedListStack/src/main.c
@@ -11,11 +11,67 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
+	printf("Unit test completed.\n");
+
 	return 0;
 }
 
 int UnitTest(void)
 {
-		int ret = 0;
-		return ret;
+	BINTREE_NODE *root_unittest = NULL;
+	STACK stack_unittest;
+
+	stack_unittest.begin = NULL;
+	stack_unittest.end = NULL;
+	root_unittest = (BINTREE_NODE *)malloc(sizeof(BINTREE_NODE));
+	
+	if (root_unittest == NULL){
+		PRINTF("Error. Malloc Fail.\n");
+		return -1;
+	}
+
+	root_unittest->data = 1;
+	root_unittest->left = NULL;
+	root_unittest->right = NULL;
+	MakeChild(root_unittest, BOTH, 2, 3);
+	MakeChild(root_unittest->left, BOTH, 4, 5);
+
+	Push(&stack_unittest, root_unittest);
+	Push(&stack_unittest, root_unittest->left);
+	Push(&stack_unittest, root_unittest->right);
+	Push(&stack_unittest, root_unittest->left->left);
+	Push(&stack_unittest, root_unittest->left->right);
+
+	if (Pop(&stack_unittest)->data != 5){
+		PRINTF("Error. 1st Pop makes an error.\n");
+		return -1;
+	}	
+	if (Pop(&stack_unittest)->data != 4){
+		PRINTF("Error. 2nd Pop makes an error.\n");
+		return -1;
+	}
+	if (Pop(&stack_unittest)->data != 3){
+		PRINTF("Error. 3rd Pop makes an error.\n");
+		return -1;
+	}
+	if (Pop(&stack_unittest)->data != 2){
+		PRINTF("Error. 4th Pop makes an error.\n");
+		return -1;
+	}
+	if (Pop(&stack_unittest)->data != 1){
+		PRINTF("Error. 5th Pop makes an error.\n");
+		return -1;
+	}
+	if (Pop(&stack_unittest) != NULL){
+		PRINTF("Error. 6th Pop makes an error.\n");
+		return -1;
+	}
+
+	free(root_unittest->left->right);
+	free(root_unittest->left->left);
+	free(root_unittest->right);
+	free(root_unittest->left);
+	free(root_unittest);
+
+	return 0;
 }
--- 0001DFS_Preorder_Using_LinkedListStack/src/mylib.c
+++ 0001DFS_Preorder_Using_LinkedListStack/src/mylib.c
@@ -14,18 +14,30 @@ BINTREE_NODE *MakeChild(BINTREE_NODE *parent, CHILD_SELECTOR selector,int leftAr
 		case NONE:
 			break;
 		case LEFT:
+			if (parent->left != NULL){
+				PRINTF("ERROR. Child already exists.\n");
+				return NULL;
+			}
 			parent->left = (BINTREE_NODE *)malloc(sizeof(BINTREE_NODE));
 			parent->left->data = leftArg;
 			parent->left->left = NULL;
 			parent->left->right = NULL;
 			break;
 		case RIGHT:
+			if (parent->right != NULL){
+				PRINTF("ERROR. Child already exists.\n");
+				return NULL;
+			}
 			parent->right = (BINTREE_NODE *)malloc(sizeof(BINTREE_NODE));
 			parent->right->data = rightArg;
 			parent->right->left = NULL;
 			parent->right->right = NULL;
 			break;
 		case BOTH:
+			if ((parent->left != NULL)||(parent->right != NULL)){
+				PRINTF("ERROR. Child already exists.\n");
+				return NULL;
+			}
 			parent->left = (BINTREE_NODE *)malloc(sizeof(BINTREE_NODE));
 			parent->left->data = leftArg;
 			parent->left->left = NULL;
@@ -45,28 +57,73 @@ BINTREE_NODE *MakeChild(BINTREE_NODE *parent, CHILD_SELECTOR selector,int leftAr
 
 int PreorderTraverse(BINTREE_NODE *root)
 {
-		int ret = 0;
-		return ret;
+	int ret = 0;
+	return ret;
 }
 
 int CleanBintree(BINTREE_NODE *root)
 {
-		int ret = 0;
-		return ret;
+	int ret = 0;
+	return ret;
 }
 
 STACK *Push(STACK *stackArg, BINTREE_NODE *bintreeArg)
 {
+	if (stackArg->begin == NULL){
+		stackArg->begin = (STACK_NODE *)malloc(sizeof(STACK_NODE));
+		if (stackArg->begin == NULL){
+			PRINTF("Error. malloc fail.\n");
+			return NULL;
+		}
+		stackArg->end = stackArg->begin;
+		stackArg->begin->data = bintreeArg;
+		stackArg->begin->next = NULL;
 		return stackArg;
+	}
+
+	stackArg->end->next = (STACK_NODE *)malloc(sizeof(STACK_NODE));
+	if (stackArg->end->next == NULL){
+		PRINTF("Error. malloc fail.\n");
+		return NULL;
+	}
+	stackArg->end->next->data = bintreeArg;
+	stackArg->end->next->next = NULL;
+	stackArg->end = stackArg->end->next;
+
+	return stackArg;
 }
 
 BINTREE_NODE *Pop(STACK *stackArg)
 {
-		BINTREE_NODE *temp = NULL;
-		return temp;
+	BINTREE_NODE *ret;
+	STACK_NODE *buf;
+
+	if (stackArg->begin == NULL){
+		return NULL;
+	}
+
+	ret = stackArg->end->data;
+
+	if (stackArg->begin == stackArg->end){
+		free(stackArg->end);
+		stackArg->end = NULL;
+		stackArg->begin = NULL;
+		return ret;
+	}
+
+	buf = stackArg->begin;
+	while (buf->next != stackArg->end)
+		buf = buf->next;
+	free(stackArg->end);
+	stackArg->end = buf;
+	
+	return ret;
 }
 
 STACK *CleanStack(STACK *stackArg)
 {
-		return stackArg;
+	while(stackArg->begin != NULL)
+		Pop(stackArg);
+
+	return stackArg;
 }
