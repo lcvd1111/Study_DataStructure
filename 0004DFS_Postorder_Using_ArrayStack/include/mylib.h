1c5521907ba15b37a4774d50c8399db3db584654
lcvd1111 <lcvd1111@gmail.com>
2023-05-21 01:39:57 +0900

    .

---
+++ 0004DFS_Postorder_Using_ArrayStack/include/mylib.h
@@ -0,0 +1,49 @@
+#ifndef __MYLIB_HEADER__
+#define __MYLIB_HEADER__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define PRINTF( x , ... ) printf("[%s, %d]" x , __func__, __LINE__, ##__VA_ARGS__)
+
+#define STACK_MAX 30
+
+//DATA TYPE DEFINITIONS
+typedef struct _BINTREE_NODE {
+	int data;
+	struct _BINTREE_NODE *left;
+	struct _BINTREE_NODE *right;
+} BINTREE_NODE;
+
+typedef enum _CHILD_SELECTOR {
+	NONE,
+	LEFT,
+	RIGHT,
+	BOTH,
+} CHILD_SELECTOR;
+
+typedef enum _LAST_ACTION {
+	TRIED_LEFT,
+	TRIED_RIGHT,
+} LAST_ACTION;
+
+typedef struct _STACK_DATA {
+	BINTREE_NODE *dataAddress;
+	LAST_ACTION dataAction;
+} STACK_DATA;
+
+typedef struct _STACK {
+	STACK_DATA stack_Array[STACK_MAX];
+	uint32_t begin;
+	uint32_t end;
+} STACK;
+
+//FUNCTION DECLARATIONS
+STACK Push(STACK, BINTREE_NODE *, LAST_ACTION);
+STACK_DATA *Pop(STACK);
+STACK EmptyStack(STACK);
+BINTREE_NODE *MakeChild(BINTREE_NODE *, CHILD_SELECTOR, int, int);
+int PostOrder(BINTREE_NODE *);
+
+#endif
---
+++ 0004DFS_Postorder_Using_ArrayStack/src/mylib.c
@@ -0,0 +1,27 @@
+#include "mylib.h"
+
+//FUNCTION DEFINITIONS
+STACK Push(STACK stackArg, BINTREE_NODE *bintreeArg, LAST_ACTION actionArg)
+{
+	return ;
+}
+
+STACK_DATA *Pop(STACK stackArg)
+{
+	return ;
+}
+
+STACK EmptyStack(STACK stackArg)
+{
+	return ;
+}
+
+BINTREE_NODE *MakeChild(BINTREE_NODE *bintreeArg, CHILD_SELECTOR actionArg, int leftArg, int rightArg)
+{
+	return ;
+}
+
+int PostOrder(BINTREE_NODE *bintreeArg)
+{
+	;
+}
