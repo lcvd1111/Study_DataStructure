a9486fbdca80a138fec630641f1b6cea06490504
lcvd1111 <lcvd1111@gmail.com>
2023-05-26 13:07:17 +0900

    .

--- 0008Array_CircularQueue_Print/src/mylib.c
+++ 0008Array_CircularQueue_Print/src/mylib.c
@@ -37,11 +37,49 @@ QUEUE *EnQueue(QUEUE* queueArg, int intArg)
 	return queueArg;
 }
 
-int DeQueue(QUEUE *queueArg, int *emptyFlag)
+int DeQueue(QUEUE *queueArg, int *emptyErr)
 {
-	//When queue is empty
-	if (queueArg->end == -1)
-		*emptyFlag = 1;
-		return -1;
+	int ret = 0;
 
+	//When queue is empty
+	if (queueArg->end == -1){
+		*emptyErr = 1;
+		return 0;
+	}
+
+	*emptyErr = 0;
+	ret = (queueArg->queueArray)[queueArg->end];
+	
+	if (queueArg->begin == queueArg->end){
+		//Setting state to 'empty state'
+		queueArg->end = -1;
+		queueArg->begin = 0;
+		return ret;
+	}
+
+	if (queueArg->begin == queueArg->length - 1)
+		queueArg->begin = 0;
+	else
+		queueArg->begin += 1;
+
+	return ret;
+	
+}
+
+QUEUE *CleanQueue(QUEUE *queueArg)
+{
+	int emptyERR = 0;
+
+	while(emptyERR == 0)
+		DeQueue(queueArg, &emptyERR);
+
+	return queueArg;
+}
+
+int DeleteQueue(QUEUE *queueArg)
+{
+	CleanQueue(queueArg);
+	free(queueArg);
+
+	return 0;
 }
