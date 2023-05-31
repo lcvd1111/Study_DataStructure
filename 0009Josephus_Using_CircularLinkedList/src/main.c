d29f575f402cf952414236aeaacacac5b85cd6ba
lcvd1111 <lcvd1111@gmail.com>
2023-05-31 17:41:59 +0900

    .

--- 0009Josephus_Using_CircularLinkedList/src/main.c
+++ 0009Josephus_Using_CircularLinkedList/src/main.c
@@ -5,10 +5,21 @@
 
 int main(int argc, char **argv)
 {
+	int n, k;
+
 	if(UnitTest()!=0){
 		return -1;
 	}
-	printf("Unit Test Success.\n");
+	printf("Unit Test Success.\n\n\n");
+
+	while(1){
+		printf("[Joshephus(n, k) = ???]\n"
+				"n=? Input an positive integer: ");
+		scanf("%d", &n);
+		printf("k=? Input an positive integer: ");
+		scanf("%d", &k);
+		printf("Josephus(%d, %d)=%d\n\n", n, k, Josephus(n, k));
+	}
 
 	return 0;
 }
