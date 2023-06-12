f29dc55ba14e045e995c8775b06afacbbd5c3acd
lcvd1111 <lcvd1111@gmail.com>
2023-06-12 09:56:49 +0900

    .

--- 0010BinaryTree_HeightFind_Using_DFS_LinkedList/include/mylib.h
+++ 0010BinaryTree_HeightFind_Using_DFS_LinkedList/include/mylib.h
@@ -8,19 +8,30 @@ typedef struct _BTREE_NODE {
 	int data;
 	struct _BTREE_NODE *left;
 	struct _BTREE_NODE *right;
+	void *header;
 } BTREE_NODE;
+
+typedef struct _HEADER_DATA {
+	int left_Sub_Height;
+	int right_Sub_Height;
+} HEADER_DATA;
 
 typedef enum _CHILD_SELECTOR {
 	LEFT,
 	RIGHT,
 	BOTH,
 	NONE,
-} CHILE_SELECTOR;
+} CHILD_SELECTOR;
+
+typedef enum _TRIED_ACTION {
+	TRIED_LEFT,
+	TRIED_RIGHT,
+	NONE,
+} TRIED_ACTION;
 
 typedef struct _STACK_NODE {
-	BTREE_NODE *nodeData;
-	int l_sub_Level;
-	int r_sub_Level;
+	BTREE_NODE *nodeAddress;
+	TRIED_ACTION *LastTried;
 	_STACK_NODE *next;
 	_STACK_NODE *prev;
 } STACK_NODE;
@@ -30,4 +41,9 @@ struct _STACK {
 	STACK_NODE *end;
 } STACK;
 
+BTREE_NODE *MakeChild(BTREE_NODE *, CHILD_SELECTOR, int Left, int Right);
+STACK_NODE *Pop(STACK *);
+STACK *Push(STACK *, BTREE_NODE *, TRIED_ACTION);
+int Calc_Subtree_Height(BTREE_NODE, CHILD_SELECTOR);
+int Calc_Btree_Height_DFS(BTREE_NODE *root);
 #endif
--- 0010BinaryTree_HeightFind_Using_DFS_LinkedList/src/main.c
+++ 0010BinaryTree_HeightFind_Using_DFS_LinkedList/src/main.c
@@ -1,3 +1,5 @@
+//My Opinion: Don't do this shit. Use BFS instead.
+
 int main(int argc, char **argv)
 {
 	return 0;
