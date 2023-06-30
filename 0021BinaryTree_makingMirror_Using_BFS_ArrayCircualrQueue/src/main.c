0bbd7d1566e4ded36bed9b0ff068802972753cfb
lcvd1111 <lcvd1111@gmail.com>
2023-07-01 01:50:10 +0900

    .

--- 0021BinaryTree_makingMirror_Using_BFS_ArrayCircualrQueue/src/main.c
+++ 0021BinaryTree_makingMirror_Using_BFS_ArrayCircualrQueue/src/main.c
@@ -3,5 +3,25 @@
 
 int main(int argc, char **argv)
 {
+	int err = 0;
+
+	if (err=UnitTest_MakeChild()){
+		printf("Unit Test for MakeChild: Fail.\n");
+		printf("Err Code: %d\n", err);
+	}
+	printf("Unit Test for MakeChild: Success.\n");
+
+	if (err=UnitTest_Queue()){
+		printf("Unit Test for Queue: Fail.\n");
+		printf("Err Code: %d\n", err);
+	}
+	printf("Unit Test for Queue: Success.\n");
+
+	if (err=UnitTest_MakeMirror()){
+		printf("Unit Test for Making Mirror: Fail.\n");
+		printf("Err Code: %d\n", err);
+	}
+	printf("Unit Test for Making Mirror: Success.\n");
+	
 	return 0;
 }
