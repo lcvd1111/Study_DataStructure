2257f67ae64774a9736123b774a3f6b6cdead718
lcvd1111 <lcvd1111@gmail.com>
2023-10-26 18:03:05 +0900

    .

---
+++ 0058Graph_Dijkstra_AdjacencyList/CPP/src/main.cc
@@ -0,0 +1,23 @@
+#include "test.hh"
+
+int main(int argc, char **argv)
+{
+	int err = 0;
+	err = UnitTest_Graph();
+	if (err){
+		std::cout << "Unit Test for Graph: FAIL" << std::endl;
+		std::cout << "Error Code: " << err << std::endl; 
+		return -1;
+	}
+	std::cout << "Unit Test for Dijkstra Algorithm: SUCCESS" << std::endl;
+
+	err = UnitTest_Dijkstra();
+	if (err){
+		std::cout << "Unit Test for Dijkstra Algorithm: FAIL" << std::endl;
+		std::cout << "Error Code: " << err << std::endl; 
+		return -2;
+	}
+	std::cout << "Unit Test for Dijkstra Algorithm: SUCCESS" << std::endl;
+
+	return 0;
+}
